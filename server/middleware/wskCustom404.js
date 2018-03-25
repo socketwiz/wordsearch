b011e0fc17f2276672def0259bc4f7b26037c962
Ricky Nelson <rickyn@socketwiz.com>
2018-03-25 10:33:23 -0400

    Update error code
    `error` may not contain the error code, we know we want to send a 404 so just do it.

--- server/middleware/wskCustom404.js
+++ server/middleware/wskCustom404.js
@@ -1,14 +1,15 @@
 
 function urlNotFound() {
     return function raiseUrlNotFoundError(req, res) {
+        const httpErrorCode = 404; // not found
         const options = {
             'root': __dirname + '../../../frontend/public'
         };
 
-        res.status(404).sendFile('404.html', options, (error) => {
+        res.status(httpErrorCode).sendFile('404.html', options, (error) => {
             if (error) {
                 console.error(error); // eslint-disable-line no-console
-                res.status(error.status).end();
+                res.status(httpErrorCode).end();
             }
         });
     };
